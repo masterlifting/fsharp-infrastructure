f70cf7b971913722bfde86236004a7d033525f99
Andrey Pestunov <masterlifting@gmail.com>
2024-08-09 23:40:59 +0200

    Refactor DSL module: remove unused code and cache union cases in Reflection module

--- src/Reflrction.fs
+++ src/Reflrction.fs
@@ -3,27 +3,3 @@ module Infrastructure.Reflection
 
 open System.Collections.Concurrent
 open Microsoft.FSharp.Reflection
-
-let private _ucCache = ConcurrentDictionary<string, obj[]>()
-
-let getUnionCases<'a> () =
-    try
-        let type' = typeof<'a>
-        let a = FSharpValue.PreComputeUnionTagReader type'
-        
-
-        // _ucCache.GetOrAdd(
-        //     type'.FullName,
-        //     fun _ ->
-        //         FSharpType.GetUnionCases(type')
-        //         |> Array.map (fun info -> FSharpValue.MakeUnion(info, args))
-        // )
-        // |> Array.map unbox<'a>
-        // |> Ok
-    with ex ->
-        let message = ex |> Exception.toMessage
-
-        Error
-        <| Operation
-            { Message = message
-              Code = Some(__SOURCE_FILE__ + __LINE__) }

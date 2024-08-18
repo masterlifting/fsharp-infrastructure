eeb8a05d6d8ae6d7df5f6d7184a8bad54c27ca16
Andrei Pestunov <masterlifting@gmail.com>
2024-08-18 22:52:46 +0200

    Refactor form data handling and enhance validation
    Refactored map filtering to use keys instead of values, renamed sets for clarity, and added validation for required keys. Updated `prepareHttpFormData` and `createRequestAppointments` to handle form data and appointments separately. Enhanced `createResult` to manage `formData`. Expanded `taskHandlers` in `Program.fs` and added utility functions `Map.reverse` and `Map.removeKeys`. Minor updates in `ConfirmationPage` and subproject commit.

--- src/DSL.fs
+++ src/DSL.fs
@@ -65,9 +65,18 @@ module Seq =
 module Map =
     let combine (map1: Map<'k, 'v>) (map2: Map<'k, 'v>) =
         map2 |> Map.fold (fun acc key value -> acc |> Map.add key value) map1
-        
+
     let removeKeys (keys: 'k list) (map: Map<'k, 'v>) =
         keys |> List.fold (fun acc key -> acc |> Map.remove key) map
+
+    let reverse (map: Map<'k, 'v>) =
+        map
+        |> Map.fold
+            (fun (acc: Map<'v, 'k list>) key value ->
+                let key' = value
+                let value' = key :: (acc |> Map.tryFind key' |> Option.defaultValue [])
+                Map.add key' value' acc)
+            Map.empty
 
 [<RequireQualifiedAccess>]
 module Graph =
@@ -132,7 +141,7 @@ module ResultAsync =
             let! result = asyncResult
             return Result.mapError f result
         }
-        
+
 [<RequireQualifiedAccess>]
 module Exception =
     let toMessage (ex: exn) =

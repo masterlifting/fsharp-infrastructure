6bb8f6fb376cb52642b0704d9c152d961def9122
Andrei Pestunov <masterlifting@gmail.com>
2025-04-02 10:34:43 +0200

    Add measures for retry policy and currency conversion

---
+++ src/domain/Measures.fs
@@ -0,0 +1,21 @@
+﻿[<AutoOpen>]
+module Infrastructure.Domain.Measures
+
+[<Measure>] type day
+[<Measure>] type attempts
+
+type RetryPolicy = { Limit: uint<attempts / day> }
+
+
+[<Measure>] type usd
+[<Measure>] type euro
+
+type Currency =
+    | USD of decimal<usd>
+    | Euro of decimal<euro>
+
+
+let convertToUsd (currency: Currency) =
+    match currency with
+    | USD amount -> amount
+    | Euro amount -> amount * 1.11m<usd/euro>

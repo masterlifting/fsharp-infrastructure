9da2a9ad7dca839bd28291a4d78157ae45293335
Andrei Pestunov <masterlifting@gmail.com>
2025-04-12 16:13:51 +0200

    Refactor logging infrastructure: introduce Client and Log modules; update Program.fs to utilize new logging methods; enhance logging provider initialization

---
+++ src/logging/providers/file/Provider.fs
@@ -0,0 +1,24 @@
+﻿module Infrastructure.Logging.Providers.File.Provider
+
+open System
+open Infrastructure.Logging.Domain
+open Infrastructure.Logging.Builder
+
+let init level =
+    let printMessage createMessage =
+        let message = createMessage <| DateTime.Now.ToString("MM-dd HH:mm:ss")
+        IO.File.AppendAllText("log.txt", message + Environment.NewLine)
+
+    let log level message =
+        let prefix =
+            match level with
+            | Critical -> "CRT"
+            | Warning -> "WRN"
+            | Debug -> "DBG"
+            | Trace -> "TRC"
+            | Success -> "SCS"
+            | _ -> "INF"
+
+        printMessage <| fun timeStamp -> $"[%s{prefix} %s{timeStamp}] %s{message}"
+
+    LoggerState <- Some(build level log)

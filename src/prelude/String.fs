53b9d8a2c798721f9be3406860144c0c251d58b4
Andrei Pestunov <masterlifting@gmail.com>
2025-04-15 02:54:29 +0200

    Add Prenotami web modules: implement initial page parsing and HTTP handling

--- src/prelude/String.fs
+++ src/prelude/String.fs
@@ -4,6 +4,7 @@ module Infrastructure.Prelude.String
 open System
 open System.Security.Cryptography
 open Infrastructure.Domain
+open System.Text
 
 let fromTimeSpan (value: TimeSpan) =
     let format = "dd\\.hh\\:mm\\:ss"
@@ -21,11 +22,14 @@ let toDefault (value: string | null) =
     | null -> String.Empty
     | v -> v
 
+let has (pattern: string) (value: string) =
+    value.Contains(pattern, StringComparison.OrdinalIgnoreCase)
+
 let toHash (value: string) =
     fun (algorithm: HashAlgorithm) ->
         try
             value
-            |> Text.Encoding.UTF8.GetBytes
+            |> Encoding.UTF8.GetBytes
             |> algorithm.ComputeHash
             |> BitConverter.ToString
             |> _.Replace("-", "").ToLower()
@@ -37,5 +41,31 @@ let toHash (value: string) =
                 Code = (__SOURCE_DIRECTORY__, __SOURCE_FILE__, __LINE__) |> Line |> Some
             }
 
-let has (pattern: string) (value: string) =
-    value.Contains(pattern, StringComparison.OrdinalIgnoreCase)
+let private generateSalt (key: int) = BitConverter.GetBytes(key) |> Array.rev
+
+let encrypt (key: int) (value: string) =
+    try
+        let salt = generateSalt key
+        let bytes = Encoding.UTF8.GetBytes(value)
+        let encryptedBytes = bytes |> Array.mapi (fun i b -> b ^^^ salt[i % salt.Length])
+        Convert.ToBase64String(encryptedBytes) |> Ok
+    with ex ->
+        Error
+        <| Operation {
+            Message = $"Encryption failed: {ex |> Exception.toMessage}"
+            Code = (__SOURCE_DIRECTORY__, __SOURCE_FILE__, __LINE__) |> Line |> Some
+        }
+
+let decrypt (key: int) (value: string) =
+    try
+        let salt = generateSalt key
+        let encryptedBytes = Convert.FromBase64String(value)
+        let decryptedBytes =
+            encryptedBytes |> Array.mapi (fun i b -> b ^^^ salt[i % salt.Length])
+        Encoding.UTF8.GetString(decryptedBytes) |> Ok
+    with ex ->
+        Error
+        <| Operation {
+            Message = $"Decryption failed: {ex |> Exception.toMessage}"
+            Code = (__SOURCE_DIRECTORY__, __SOURCE_FILE__, __LINE__) |> Line |> Some
+        }

ae4e8923babff4e0645eb71e6e4bf2dcb3a80d88
Andrei <masterlifting@Gmail.com>
2025-10-03 23:15:43 +0200

    Refactor: improve Node type methods and enhance search functionality

--- src/prelude/Tree.fs
+++ src/prelude/Tree.fs
@@ -1,6 +1,7 @@
 ﻿[<RequireQualifiedAccess>]
 module Infrastructure.Prelude.Tree
 
+open System
 open Infrastructure.Domain
 
 /// <summary>
@@ -44,14 +45,13 @@ module BFS =
 
         [ tree ] |> search
 
-
 type Node<'T> private (id: string, value: 'T, children: ResizeArray<Node<'T>>) =
-    
+
     member _.Id = id
     member _.Value = value
-    member _.Children : Node<'T> seq = children :> Node<'T> seq
+    member _.Children: Node<'T> seq = children :> Node<'T> seq
 
-    static member Create(id: string, value: 'T) =
+    static member create(id: string, value: 'T) =
         Node(id, value, ResizeArray<Node<'T>>())
 
     member private _.Add(child: Node<'T>) =
@@ -66,47 +66,38 @@ type Node<'T> private (id: string, value: 'T, children: ResizeArray<Node<'T>>) =
         children |> Seq.iter this.Add
         this
 
-type Root<'T> =
-    {
-        Root: Node<'T>
-        Delimiter: char
-    }
-
-    static member Init(root: Node<'T>) =
-        {
-            Root = root
-            Delimiter = '.'
-        }
-
     member this.FindNode(id: string) =
-        if System.String.IsNullOrWhiteSpace id then
+        if String.IsNullOrWhiteSpace id then
             None
         else
-            let ids = id.Split(this.Delimiter, System.StringSplitOptions.RemoveEmptyEntries)
+            let parts = id.Split('.', StringSplitOptions.RemoveEmptyEntries)
 
-            if ids.Length = 0 || ids.[0] <> this.Root.Id then
+            if parts.Length = 0 || parts.[0] <> this.Id then
                 None
             else
-                this.FindRecursive(this.Root, ids, 1)
+                this.FindRecursive(this, parts, 1)
 
     member this.FindValue(id: string) =
-       this.FindNode id
-       |> Option.map (fun (v: Node<'T>) -> v.Value)
+        this.FindNode id |> Option.map (fun (v: Node<'T>) -> v.Value)
 
-    member this.Contains(path: string) =
-        this.FindValue(path).IsSome
-
-    member private this.FindRecursive(node: Node<'T>, parts: string[], index: int) =
-        if index >= parts.Length then
-            Some node
+    member this.Contains(path: string) = this.FindValue(path).IsSome
+    
+    member private this.FindRecursive(current: Node<'T>, ids: string[], index: int) =
+        if index >= ids.Length then
+            Some current
         else
-            let child = node.Children |> Seq.tryFind (fun c -> c.Id = parts.[index])
-            match child with
-            | Some c -> this.FindRecursive(c, parts, index + 1)
-            | None -> None
+            current.Children
+            |> Seq.tryFind (fun c -> c.Id = ids.[index])
+            |> Option.bind (fun child -> this.FindRecursive(child, ids, index + 1))
 
 module NodeBuilder =
-    
+
     let withChild (child: Node<'T>) (parent: Node<'T>) = parent.AddChild child
 
     let withChildren (children: Node<'T> seq) (parent: Node<'T>) = parent.AddChildren children
+
+    let findNode (nodeId: string) =
+        fun (node: Node<'T>) -> node.FindNode nodeId
+
+    let findValue (nodeId: string) =
+        fun (node: Node<'T>) -> node.FindValue nodeId

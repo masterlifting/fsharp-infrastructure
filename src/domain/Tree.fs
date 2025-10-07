a5d567b8116eb556c6afc3d73fe3d2d26a3b8590
Andrei Pestunov <masterlifting@gmail.com>
2025-10-07 20:05:22 +0200

    Refactor: enhance Node structure with parent-child relationships and improve ID handling

--- src/domain/Tree.fs
+++ src/domain/Tree.fs
@@ -3,17 +3,40 @@ module Infrastructure.Domain.Tree
 
 open System
 
-type Node<'T> private (id: string, value: 'T, children: ResizeArray<Node<'T>>) =
+[<Literal>]
+let private Delimiter = '.'
 
-    member _.Id = id
+[<RequireQualifiedAccess>]
+type NodeId =
+    | NodeId of string
+
+    member this.Value =
+        match this with
+        | NodeId id -> id
+
+    static member create value =
+        NodeId value
+
+    static member split (NodeId id) =
+        id.Split(Delimiter, StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+
+type Node<'T> private (id: string, value: 'T, parent: Node<'T> option, children: ResizeArray<Node<'T>>) =
+    member private _.CurrentId = id
+    member _.Id: NodeId =
+        match parent with
+        | Some p -> $"%s{p.Id.Value}%c{Delimiter}%s{id}" |> NodeId.create
+        | None -> id |> NodeId.create
     member _.Value = value
+    member _.Parent = parent
     member _.Children: Node<'T> seq = children :> Node<'T> seq
 
     static member create(id: string, value: 'T) =
-        Node(id, value, ResizeArray<Node<'T>>())
+        Node(id, value, None, ResizeArray<Node<'T>>())
 
-    member private _.Add(child: Node<'T>) =
-        if not (children |> Seq.exists (fun c -> c.Id = child.Id)) then
+    member private this.Add(child: Node<'T>) =
+        if not (children |> Seq.exists (fun c -> c.CurrentId = child.CurrentId)) then
+            let child = Node(child.CurrentId, child.Value, Some this, child.Children |> ResizeArray)
             children.Add child
 
     member internal this.AddChild(child: Node<'T>) =
@@ -28,9 +51,9 @@ type Node<'T> private (id: string, value: 'T, children: ResizeArray<Node<'T>>) =
         if String.IsNullOrWhiteSpace id then
             None
         else
-            let parts = id.Split('.', StringSplitOptions.RemoveEmptyEntries)
+            let parts = id.Split(Delimiter, StringSplitOptions.RemoveEmptyEntries)
 
-            if parts.Length = 0 || parts.[0] <> this.Id then
+            if parts.Length = 0 || parts.[0] <> this.CurrentId then
                 None
             else
                 this.FindRecursive(this, parts, 1)
@@ -45,5 +68,5 @@ type Node<'T> private (id: string, value: 'T, children: ResizeArray<Node<'T>>) =
             Some current
         else
             current.Children
-            |> Seq.tryFind (fun c -> c.Id = ids.[index])
+            |> Seq.tryFind (fun c -> c.CurrentId = ids.[index])
             |> Option.bind (fun child -> this.FindRecursive(child, ids, index + 1))

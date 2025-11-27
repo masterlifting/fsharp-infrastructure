b4cf5c0cda384dbad0ee185427f7ab2e2d284717
Andrei Pestunov <masterlifting@gmail.com>
2025-11-27 20:44:59 +0100

    Refactor: Update string formatting in fromTimeSpan and fromDateTime functions for improved readability

--- src/prelude/String.fs
+++ src/prelude/String.fs
@@ -8,11 +8,11 @@ open System.Text
 
 let fromTimeSpan (value: TimeSpan) =
     let format = "dd\\.hh\\:mm\\:ss"
-    value.ToString(format)
+    value.ToString format
 
 let fromDateTime (value: DateTime) =
     let format = "yyyy-MM-dd HH:mm:ss"
-    value.ToString(format)
+    value.ToString format
 
 let addLines count =
     Seq.init count (fun _ -> Environment.NewLine) |> String.concat ""
